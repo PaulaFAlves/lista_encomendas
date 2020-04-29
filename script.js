47c34c9f59fd6c0c3f7ec85b8d86ae0c5434cec8
Paula Alves <paulafernandaalves@icloud.com>
2020-04-28 23:25:31 -0300

    started project

---
+++ script.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+    $.ajax({
+        url: '/server.json',
+        method: 'get',
+        dataType: 'json',
+        success: function(data) {
+            console.log(data)
+            let valorFinal = []
+            len = data.encomendas.length;
+            console.log(len)
+            for (i = 0; i < len;i++) {
+                valorFinal.push((data.encomendas[i].valor * 1.043).toFixed(2));
+            }
+            console.log(valorFinal)
+            let exampleTable = $('#example')
+                .DataTable({
+                    data: data.encomendas,
+                        'aaSorting': [[1, 'asc']],
+                    dom: "<'row'<'col-md-6 text-left'T><'col-md-6 text-right'f>>" +
+                        "<'row'<'col-md-12't>>" +
+                        "<'row'<'col-md-6'i><'col-md-6'p>>",
+                        'columns': [
+                            { 'data': 'codigo' },  
+                            { 'data': 'cliente.nome' },
+                            { 'data': 'valor' },
+                            { 'data': 'valor' } ,
+                            { 'data': 'data' },       
+                    ],
+                    language: {
+                        "sZeroRecords": "Nenhum registro encontrado",
+                    }
+                });
+        }
+    });
+});
+
